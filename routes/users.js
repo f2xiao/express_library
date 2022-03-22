d0abf609f184b36a86dcad717ed66f654b6ce954
f2xiao <fxiao.mecheng@gmail.com>
2022-03-22 15:26:39 -0400

    Add controllers files

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
-router.get('/cool', function(req, res, next) {
+router.get('/cool', function(req, res) {
   res.send("You're so cool");
 });
 
