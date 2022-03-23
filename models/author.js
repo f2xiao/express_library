7432bafd1a3bfd90039348b0cd342d196581ebef
f2xiao <fxiao.mecheng@gmail.com>
2022-03-23 16:27:07 -0400

    Add bookinstance_list, author_list and genre_list templates

--- models/author.js
+++ models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema(
@@ -17,7 +17,7 @@ AuthorSchema
 .get(function () {
 // To avoid errors in cases where an author does not have either a family name or first name
 // We want to make sure we handle the exception by returning an empty string for that case
-  const fullname = '';
+  let fullname = '';
   if (this.first_name && this.family_name) {
     fullname = this.family_name + ', ' + this.first_name
   }
@@ -29,15 +29,16 @@ AuthorSchema
 
 // Virtual for author's lifespan
 AuthorSchema.virtual('lifespan').get(function() {
-  const lifetime_string = '';
-  if (this.date_of_birth) {
-    lifetime_string = this.date_of_birth.getYear().toString();
-  }
-  lifetime_string += ' - ';
-  if (this.date_of_death) {
-    lifetime_string += this.date_of_death.getYear()
-  }
-  return lifetime_string;
+  // let lifetime_string = '';
+  // if (this.date_of_birth) {
+  //   lifetime_string = this.date_of_birth.getYear().toString();
+  // }
+  // lifetime_string += ' - ';
+  // if (this.date_of_death) {
+  //   lifetime_string += this.date_of_death.getYear()
+  // }
+  // return lifetime_string;
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : ''
 });
 
 // Virtual for author's URL
