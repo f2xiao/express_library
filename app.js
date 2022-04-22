d4fe9ae9ac7f3aa20997ce9b9b818620cd7ccc85
f2xiao <fxiao.mecheng@gmail.com>
2022-04-22 14:27:03 -0400

    Edit app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -12,7 +14,7 @@ const app = express();
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://pixie:3TpO49E55UtO204o@cluster0.pcocp.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.pcocp.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
