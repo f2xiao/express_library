2382a25fafd41b5003ad63c936c75eecaab2de06
f2xiao <fxiao.mecheng@gmail.com>
2022-04-26 19:34:56 -0400

    Add two virtual properties in author model

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -144,8 +144,19 @@ exports.author_delete_post = function(req, res, next) {
 
 
 // Display Author update form on GET.
-exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
+exports.author_update_get = function(req, res, next) {
+    Author.findById(req.params.id)
+        .exec(function (err, author) {
+            if (err) { return next(err); }
+            if (author==null) { // No results.
+                var err = new Error('Book not found');
+                err.status = 404;
+                return next(err);
+            }
+
+            // Success.
+            res.render('author_form', { title: 'Update author', author: author });
+        })
 };
 
 // Handle Author update on POST.
--- models/author.js
+++ models/author.js
@@ -53,6 +53,22 @@ AuthorSchema
   return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
+// Virtual for author's date_of_birth_ISODate
+AuthorSchema
+.virtual('date_of_birth_ISODate')
+.get(function () {
+  return this.date_of_birth ? `${DateTime.fromJSDate(this.date_of_birth).toISODate()}` : '';
+});
+
+// Virtual for author's date_of_death_ISODate
+AuthorSchema
+.virtual('date_of_death_ISODate')
+.get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toISODate() : '';
+});
+
+
+
 
 
 //Export model
